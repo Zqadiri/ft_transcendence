f0b5a6dd7f1499053869a8d1e4286533161f2b3a
Ayoub Amzouar <aamzouar@e1r13p5.1337.ma>
2022-07-24 10:33:04 +0100

    i don't need these

--- src/games/games.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GameController } from './games.controller';
-import { GamesService } from './games.service';
-
-@Module({
-  controllers: [GameController],
-  providers: [GamesService]
-})
-export class GameModule {}
--- src/players/players.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('players')
-export class PlayersController {
-
-}
