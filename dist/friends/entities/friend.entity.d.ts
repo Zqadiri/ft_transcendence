e9ee3925c60e11d352b0b6715b66631d9445047f
Zqadiri <zinebghost@gmail.com>
2022-08-06 11:21:23 +0100

    clean branch

--- dist/friends/entities/friend.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "src/users/entities/user.entity";
-import { BaseEntity } from "typeorm";
-export declare class Friend extends BaseEntity {
-    id: number;
-    blocked: boolean;
-    stat: string;
-    user: User | null;
-}
--- dist/friends/relation.repository.d.ts
+++
@@ -1,5 +0,0 @@
-import { Repository } from "typeorm";
-import { Friend } from "src/friends/entities/friend.entity";
-declare class relationRepository extends Repository<Friend> {
-}
-export { relationRepository };
