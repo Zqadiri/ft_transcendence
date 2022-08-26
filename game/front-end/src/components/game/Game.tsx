f2ff2743e186093541935fabd05a85c257763728
Ayoub Amzouar <gramgeek01@gmail.com>
2022-08-26 17:45:57 +0100

    live games design is done

--- game/front-end/src/components/game/Game.tsx
+++ game/front-end/src/components/game/Game.tsx
@@ -4,20 +4,41 @@ import "./Style.css";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { SyntheticEvent, useState } from "react";
 
-function	GameNavBar(): JSX.Element
-{
-	return (
-			<nav className="game-nav">
-				<Link to="/" style={{backgroundColor: "#F66B0E"}}>Play Game</Link>
-				<Link to="/live" >Watch Game</Link>
-			</nav>
-	);
-}
+const	GamesData = [
+	{user1: "Sickl", user2: "Sesco", score1: 3, score2: 1, avatar1: "https://cdn.intra.42.fr/users/small_isaadi.jpg", avatar2: "https://cdn.intra.42.fr/users/small_aamzouar.jpg", id: 1},
+	{user1: "Zineb", user2: "Sara", score1: 6, score2: 8, avatar1: "https://cdn.intra.42.fr/users/small_zqadiri.jpg", avatar2: "https://cdn.intra.42.fr/users/small_sbensarg.jpg", id: 2},
+	{user1: "Sara", user2: "Sickl", score1: 1, score2: 5, avatar1: "https://cdn.intra.42.fr/users/small_sbensarg.jpg", avatar2: "https://cdn.intra.42.fr/users/small_isaadi.jpg", id: 3},
+	{user1: "Sesco", user2: "Zineb", score1: 5, score2: 4, avatar1: "https://cdn.intra.42.fr/users/small_aamzouar.jpg", avatar2: "https://cdn.intra.42.fr/users/small_zqadiri.jpg", id: 4},
+	{user1: "Sara", user2: "Sesco", score1: 0, score2: 0, avatar1: "https://cdn.intra.42.fr/users/small_sbensarg.jpg", avatar2: "https://cdn.intra.42.fr/users/small_aamzouar.jpg", id: 5},
+];
 
 function	LiveGames(): JSX.Element
 {
 	return (
 		<>
+			<ul className="live-games">
+				{
+					GamesData.map((current) => {
+						return (
+							<li>
+								<div>
+									<img src={current.avatar1} alt="user avatar"/>
+									<h3>{current.user1}</h3>
+								</div>
+								<div>
+									<h3>{current.score1}</h3>
+									<h3>-</h3>
+									<h3>{current.score2}</h3>
+								</div>
+								<div>
+									<img src={current.avatar2} alt="user avatar"/>
+									<h3>{current.user2}</h3>
+								</div>
+							</li>
+						);
+					})
+				}
+			</ul>
 		</>
 	);
 }
