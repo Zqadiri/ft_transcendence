6fd4c3b32bcdcde60ed6603192b2402e27c8321b
Ayoub Amzouar <gramgeek01@gmail.com>
2022-08-30 18:24:22 +0100

    matching back-end

--- game/back-end/src/app.module.ts
+++ game/back-end/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GameGateway } from './game.gateway';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [GameGateway],
 })
 export class AppModule {}
