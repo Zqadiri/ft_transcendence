e4ed94b21f4c3cffc163deb03f3b4089ec55995a
Ayoub Amzouar <aamzouar@student.1337.ma>
2022-10-09 21:44:16 +0100

    now user match-history is on the right side

--- frontend/src/components/UserProfile.tsx
+++ frontend/src/components/UserProfile.tsx
@@ -351,7 +351,7 @@ const UserProfile = (props: { self: boolean }) => {
 							</ShowConditionally>
 						</div>
 					{/* </div> */}
-					<div className="usergameinfo flex-column flex-gap20">
+					<div className="usergameinfo flex-column">
 						<div className="header">
 							<h1>Match History</h1>
 							<div className="userstats">
@@ -373,9 +373,9 @@ const UserProfile = (props: { self: boolean }) => {
 													<h3>{game.firstPlayerID}</h3>
 												</div>
 												<div className="match-results">
-													<span>{game.firstPlayerScore}</span>
-													<span> vs </span>
-													<span>{game.firstPlayerScore}</span>
+													<h3>{game.firstPlayerScore}</h3>
+													<h3> - </h3>
+													<h3>{game.firstPlayerScore}</h3>
 												</div>
 												<div className="secondPlayeer">
 													<div className="avatar"></div>
