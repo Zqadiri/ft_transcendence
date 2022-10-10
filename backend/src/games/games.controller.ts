098029d595ea289e255a6d10ffa58efddea5e5ed
Zqadiri <zinebghost@gmail.com>
2022-10-10 02:00:46 +0100

    Merge remote-tracking branch 'origin/merge-branch'

--- backend/src/games/games.controller.ts
+++ backend/src/games/games.controller.ts
@@ -4,7 +4,6 @@ import { Game } from './entities/game.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { GameRepository } from './game.repository';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { UsersService } from 'src/users/users.service';
 import RequestWithUser from 'src/two-factor-authentication/dto/requestWithUser.interface';
 import { jwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
@@ -16,9 +15,8 @@ export class GameController {
 		@InjectRepository(Game)
 		private readonly gameRepo: GameRepository,
 		private readonly gameServ: GamesService,
-		private readonly userServ: UsersService
-	) { }
 
+	) { }
 
 	@Get('/get_match_history')
 	async GetMatchHistory(@Query() query: { id: number, name: string }, @Req() req: RequestWithUser)
